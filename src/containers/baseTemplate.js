bbc3175afbcf8a9fe0cafe5f6dda349bdb354738
Matthew <Matbei14@yandex.ru>
2021-03-02 07:05:18 +0300

    add main background

---
+++ src/containers/baseTemplate.js
@@ -0,0 +1,13 @@
+import "../themes/baseTemplate.css";
+
+function BaseTemplate() {
+  return (
+    <div className="main">
+      <div className="main_heading">
+        <p className="main_heading_name">PhotoGallery</p>
+      </div>
+    </div>
+  );
+}
+
+export default BaseTemplate;
