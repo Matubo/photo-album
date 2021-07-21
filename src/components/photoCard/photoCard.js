068d1b9129057b7107db3c2c615a18662a541172
Matthew <Matbei14@yandex.ru>
2021-07-21 18:53:47 +0300

    refact some code/photoCard component fixed and reduced

--- src/components/photoCard/photoCard.js
+++ src/components/photoCard/photoCard.js
@@ -2,42 +2,24 @@ import './photoCard.css';
 
 function PhotoCard(props) {
   const { length, callback, url, id, title } = props;
-  if (length) {
-    return (
-      <div className="card photo_card">
-        <div className="card__length">{length}</div>
-        <div
-          className="card__img-active"
-          style={{
-            backgroundImage: `url(${url})`,
-          }}
-          onClick={() => {
-            callback(id);
-          }}
-        ></div>
-        <p className="card__title" title={title}>
-          {title}
-        </p>
-      </div>
-    );
-  } else {
-    return (
-      <div className="card photo_card">
-        <div
-          className="card__img"
-          style={{
-            backgroundImage: `url(${url})`,
-          }}
-          onClick={() => {
-            callback(id);
-          }}
-        ></div>
-        <p className="card__title" title={title}>
-          {title}
-        </p>
-      </div>
-    );
-  }
+
+  return (
+    <div className="card photo_card">
+      {length ? <div className="card__length">{length}</div> : <></>}
+      <div
+        className={length ? 'card__img card__img-active' : 'card__img'}
+        style={{
+          backgroundImage: `url(${url})`,
+        }}
+        onClick={() => {
+          callback(id);
+        }}
+      ></div>
+      <p className="card__title" title={title}>
+        {title}
+      </p>
+    </div>
+  );
 }
 
 export default PhotoCard;
